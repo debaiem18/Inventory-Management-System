dc4b2df44e2a7d73f3633a7c3cef1d827985fb65
DEBAYUDH MUKHERJEE <debayudhmukherjeek34@gmail.com>
2026-03-18 11:26:19 +0530

    FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -1,47 +1,27 @@
-import java.io.*;
 import java.util.*;
 
-public class FileManager {
-
-    private static final String FILE_NAME = "products.txt";
-
-    public static void saveProducts(List<Product> products) {
-
-        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME))) {
-
-            for (Product p : products) {
-                writer.println(p.toString());
-            }
-
-        } catch (IOException e) {
-            System.out.println("Error saving file.");
-        }
-    }
-
-    public static List<Product> loadProducts() {
-
-        List<Product> products = new ArrayList<>();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
-
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-
-                String[] data = line.split(",");
-
-                int id = Integer.parseInt(data[0]);
-                String name = data[1];
-                double price = Double.parseDouble(data[2]);
-                int qty = Integer.parseInt(data[3]);
-
-                products.add(new Product(id, name, price, qty));
-            }
-
-        } catch (IOException e) {
-            System.out.println("File not found, starting new inventory.");
-        }
-
-        return products;
-    }
+public class ProductDAOImpl implements ProductDAO {
+private List<Product> products;
+public ProductDAOImpl() {
+products = FileManager.loadProducts();
+}
+public void addProduct(Product product) {
+products.add(product);
+FileManager.saveProducts(products);
+}
+public List<Product> getAllProducts() {
+return products;
+}
+public void updateProduct(int id, int quantity) {
+for (Product p : products) {
+if (p.getId() == id) {
+p.setQuantity(quantity);
+}
+}
+FileManager.saveProducts(products);
+}
+public void deleteProduct(int id) {
+products.removeIf(p -> p.getId() == id);
+FileManager.saveProducts(products);
+}
 }
